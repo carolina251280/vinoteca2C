ae274eded52c75c19ddf214c345cf5e1a14f5541
carolina251280 <carolinamarquez.gcg@gmail.com>
2023-04-23 03:10:08 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+var vinos = vinoteca.vinos;
+var vinosTintos = [];
+var vinosRosados = [];
+var vinosBlancos = [];
+var vinosEspumantes = [];
+
+for (var i = 0; i < vinos.length; i++){
+    if (vinos[i].category == "Vinos tintos"){
+        vinosTintos.push(vinos[i])
+    }
+    else if (vinos[i].category == "Vinos rosados"){
+            vinosRosados.push(vinos[i])
+        }
+    else if (vinos[i].category == "Vinos blancos"){
+        vinosBlancos.push(vinos[i])
+    }
+    else if (vinos[i].category == "Vinos espumantes"){
+        vinosEspumantes.push(vinos[i])
+    }
+
+}
+
+var botonNav = document.getElementsByClassName("boton")
+
+for (var i = 0; i < botonNav.length; i++){
+    const element = botonNav[i];
+    element.addEventListener("click", function(e){
+        imprimir(e.target.id);
+    })
+}
+
+function imprimir(id){
+    switch(id){
+        case "vinosTintos": 
+            console.log("Estos son los vinos tintos");
+            display(vinosTintos);
+            document.getElementById("tituloPrincipal").innerHTML = "Vinos tintos";
+            break;
+
+        case "vinosRosados": 
+            console.log("Estos son los vinos rosados");
+            display(vinosRosados);
+            document.getElementById("tituloPrincipal").innerHTML = "Vinos Rosados";
+            break;
+
+
+        case "vinosBlancos": 
+            console.log("Estos son los vinos blancos");
+            display(vinosBlancos);
+            document.getElementById("tituloPrincipal").innerHTML = "Vinos Blancos";
+            break;
+
+
+        case "vinosEspumantes": 
+            console.log("Estos son los vinos espumantes");
+            display(vinosEspumantes);
+            document.getElementById("tituloPrincipal").innerHTML = "Vinos Espumantes";
+            break;
+
+        default:
+            console.log("Estos son todos los vinos");
+            display(vinos);
+            document.getElementById("tituloPrincipal").innerHTML = "Vinos";
+    }
+    
+}
+
+function display (array){
+    var html = "";
+    
+    for(i = 0; i < array.length; i++){
+        html += `
+        <div class="product">
+        <img src="./img/${array[i].image}" alt="Vino tinto 1">
+        <h3>${array[i].name}</h3>
+        <p>${array[i].description}</p>
+        <p class="price">$ ${array[i].price}</p>
+        <button>Agregar al carrito</button>
+    </div>
+         `
+    }
+    document.getElementById("contenedor").innerHTML = html;
+}
+imprimir("home");
---
+++ js/vinoteca.js
@@ -0,0 +1,139 @@
+const vinoteca = {
+    "vinos":[
+      {
+      "id": 1,
+      "image":"1.jpg",
+      "name": "Familia Blanco Blend",
+      "date": "2021-12-12",
+      "state":"En stock",
+      "bodega":"Familia Blanco",
+      "description": "Malbec, Bonarda y Cabernet Sauvignon sellados con una sutil presencia de Cabernet Franc dan vida a un blend de excelencia. Refinado, distinguido y complejo.",
+      "category":"Vinos tintos",
+      "price": 11800
+    },
+    {
+      "id": 2,
+      "image":"2.jpg",
+      "name": "Bo Bó Bohemian Zorrito Mb",
+      "date": "2021-11-02",
+      "state":"En stock",
+      "bodega":"Bo-Bó Wines",
+      "description": "Un Malbec fresco, rústico, salvaje, indómito: vinificado con levaduras naturales , buscando reflejar lo mas puro del espíritu del Malbec de nuestro viñedo directo de la botella. Con una crianza breve en barricas de tercer uso, solo se busco el aporte de micro oxigenación y el soporte necesario para darnos como reseultado un Malbec muy actual, jugoso y vibrante.",
+      "category":"Vinos tintos",
+      "price": 3800
+    },
+    {
+      "id": 3,
+      "image":"3.jpg",
+      "name": "Piel Negra",
+      "date": "2022-02-12",
+      "state":"En stock",
+      "bodega":"Bressia",
+      "description": "Este varietal 100% pinot noir sorprende por la tonalidad e intensidad en su aspecto visual ya que ofrece un color rojo profundo muy intenso con matices negros de gran atractivo y vivacidad.",
+      "category": "Vinos tintos",
+      "price": 8500
+    },
+
+      {
+      "id": 4,
+      "image":"4.jpg",
+      "name": "Domiciano Mistela",
+      "date": "2022-01-22",
+      "state":"En stock",
+      "bodega":"Domiciano",
+      "description": "Esta MISTELA DOMICIANO es elaborada a partir de una selección de uvas Moscatel y Torrontes. Una vez cortadas con alcohol vinico, son criadas por 4 años en barricas de roble frances para logra la complejidad aromatica y color caramelo que la caracterizan.",
+      "category": "Vinos rosados",
+      "price": 3750
+    },
+    {
+      "id": 5,
+      "image":"5.jpg",
+      "name": "El Esteco Old Vines Criolla",
+      "date": "2021-03-01",
+      "state":"En stock",
+      "bodega":"El Esteco",
+      "description": "El Esteco Old Vines es un homenaje a la vida y trayectoria de algunas de nuestras fincas con más de 50 años de antigüedad. Viñedos que viven en armonía con el terroir y precisan la mínima intervención del hombre para regalarnos vinos excepcionales.",
+      "category": "Vinos rosados",
+      "price": 5850
+    },
+    {
+      "id": 6,
+      "image":"6.jpg",
+      "name": "Angélica Zapata",
+      "date": "2021-10-15",
+      "state":"Agotado",
+      "bodega":"Catena Zapata",
+      "description": "Angélica Chardonnay es un “single vineyard” que refleja características propias de la zona de gran altitud en donde se origina. ",
+      "category": "Vinos rosados",
+      "price": 4320
+    },
+
+      {
+      "id": 7,
+      "image":"7.jpg",
+      "name": "Verum",
+      "date": "2022-10-15",
+      "state":"En stock",
+      "bodega":"Del Rio Elorza",
+      "description": "Un Chardonnay 100% bajo el estilo del mejor Borgoña",
+      "category": "Vinos blancos",
+      "price": 3700
+    },
+    {
+      "id": 8,
+      "image":"8.jpg",
+      "name": "Fond de Cave Reserva Cha",
+      "date": "2022-10-15",
+      "state":"En stock",
+      "bodega":"Trapiche",
+      "description": "Muy elegante, este vino despliega aromas a manzanas rojas, peras, miel y canela. De gran frescura, es untuoso y posee un carácter mineral. En boca presenta un suave final.",
+      "category": "Vinos blancos",
+      "price": 2650
+    },
+    {
+      "id": 9,
+      "image":"9.jpg",
+      "name": "Marchiori & Barraud",
+      "date": "2022-10-15",
+      "state":"En stock",
+      "bodega":"Marchiori & Barraud",
+      "description": "Es un Chardonnay voluminoso y largo en su paso. Especial para acompañar carnes de aves de corral, con salsas a base de cremas y alcaparras",
+      "category": "Vinos blancos",
+      "price": 2500
+    },
+
+      {
+        "id": 10,
+        "image":"10.jpg",
+        "name": "Moet & Chandon Imperial",
+        "state":"Agotado",
+        "date": "2022-10-15",
+        "bodega":"Moet & Chandon",
+        "description": "Moët & Chandon Impérial es el champán icónico de la Maison. Creado en 1869, encarna el estilo único de Moët & Chandon, un estilo que se distingue por su frutado brillante, su paladar seductor y su elegante madurez.",
+        "category": "Vinos espumantes",
+        "price": 33600
+      },
+      {
+        "id": 11,
+        "image":"11.jpg",
+        "name": "Dom Pérignon Vintage 2009",
+        "state":"En stock",
+        "bodega":"Dom Pérignon",
+        "date": "2022-10-15",
+        "description": "Dom Pérignon Vintage se elabora exclusivamente a partir de las mejores uvas de un único año, reinventándose a sí mismo mediante la interpretación del singular carácter de las estaciones y atreviéndose a no elaborar un Vintage si la cosecha no cumple las elevadas expectativas.",
+        "category": "Vinos espumantes",
+        "price": 55000
+      },
+      {
+        "id": 12,
+        "image":"12.jpg",
+        "name": "Veuve Clicquot Brut “Yellow Label”",
+        "date": "2022-10-15",
+        "state": "En stock",
+        "bodega":"Veuve Clicquot",
+        "description": "Veuve Clicquot Yellow Label es un ejemplo perfecto de armonía entre delicadeza y energía. Dominada por Pinot Noir, este vino tiene una estructura firme, redondeada con un toque de Pinot Meunier. Casi un tercio de Chardonnay le da la elegancia y delicadeza necesaria para un equilibrio perfecto.",
+        "category": "Vinos espumantes",
+        "price": 35500
+      }
+    ]
+}
